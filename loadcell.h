97a26ec35bfef809a08e38402b3ed8759e670ee2
mansour9897 <mansour9897@gmail.com>
2022-06-27 08:41:16 +0430

    add loadcell header file

---
+++ loadcell.h
@@ -0,0 +1,22 @@
+#ifndef _loadcell_h_
+#define _loadcell_h_
+
+#include "ads1230.h"
+
+class loadcell
+{
+private:
+    ADS1230 ads;
+    float capacity;
+    float vExc;
+    float mvPerVolt;
+
+public:
+    loadcell(ADS1230 pAds,
+             float pCapacity /* Loadcell capacity */,
+             float pVExc, /* Excitation voltage */
+             float pMvPerVolt /* loadcell mv/v */);
+    ~loadcell();
+};
+
+#endif // !_loadcell_h_
